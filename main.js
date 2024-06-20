06f34c829ba6e973dd69c288ae27f6ee533cc923
fantastik8971 <161023430+fantastik8971@users.noreply.github.com>
2024-06-20 18:14:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const cars = {
+    nomi:"BMW",
+    narxi :15000,
+    rangi:"qora",
+    yili:2009,
+
+}
+
+for(bot in cars){
+    console.log(bot , ":" , cars[bot]);
+}
+
+
+// let userName = prompt("ismingizni kiriting");
+// let newText = document.createElement("h1");
+// newText.textContent = userName;
+// newText.className = "bot"
+
+
+// newText.innerHTML = `<h2 style="color:red"  class="salom" >${userName}</h2>`
+
+
+// // newText.style.backgroundColor = "red"
+// // newText.style.fontSize = "50px"
+// // newText.style.textAlign = "center"
+// // newText.style.color = "white"
+// document.body.appendChild(newText)
+// console.log(newText);
